365e2a7826be12a3bd47ebdc6729bbfd2c78ad38
Elias N Vasylenko <eliasvasylenko@gmail.com>
2019-06-09 14:30:26 +0100

    added empty glsp project

---
+++ derivation-glsp/src/main/java/org/topiello/derivation/glsp/package-info.java
@@ -0,0 +1,20 @@
+/*
+ * Topiello Derivation GLSP - The derivation-glsp API bundle - built using OSGi enRoute R7
+ * Copyright © 2018 Strange Skies (elias@vasylenko.uk)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+@org.osgi.annotation.bundle.Export
+@org.osgi.annotation.versioning.Version("1.0.0")
+package org.topiello.derivation.glsp;
