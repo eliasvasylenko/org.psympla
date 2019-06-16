d3aac02a96f00c2ce0789b716d8756011cde478b
Elias N Vasylenko <eliasvasylenko@gmail.com>
2019-06-16 12:12:34 +0100

    missed from previous commit

--- lexicon/src/main/java/org/topiello/lexicon/scanning/Scan.java
+++ lexicon/src/main/java/org/topiello/lexicon/scanning/Scan.java
@@ -1,5 +1,20 @@
 /*
  * Topiello Lexicon - An API for creating simple lexicons to be consumed by grammars
+ *     __   _______  ____           _       __     _      __       __
+ *   ,`_ `,|__   __||  _ `.        / \     |  \   | |  ,-`__`¬  ,-`__`¬
+ *  ( (_`-'   | |   | | ) |       / . \    | . \  | | / .`  `' / .`  `'
+ *   `._ `.   | |   | |-. L      / / \ \   | |\ \ | || |    _ | '-~.
+ *  _   `. \  | |   | |  `.`.   / /   \ \  | | \ \| || |   | || +~-'
+ * \ \__.' /  | |   | |    \ \ / /     \ \ | |  \ ` | \ `._' | \ `.__,.
+ *  `.__.-`   |_|   |_|    |_|/_/       \_\|_|   \__|  `-.__.J  `-.__.J
+ *                  __    _         _      __      __
+ *                ,`_ `, | |  _    | |  ,-`__`¬  ,`_ `,
+ *               ( (_`-' | | ) |   | | / .`  `' ( (_`-'
+ *                `._ `. | L-' L   | || '-~.     `._ `.
+ *               _   `. \| ,.-^.`. | || +~-'    _   `. \
+ *              \ \__.' /| |    \ \| | \ `.__,.\ \__.' /
+ *               `.__.-` |_|    |_||_|  `-.__.J `.__.-`
+ *
  * Copyright © 2018 Strange Skies (elias@vasylenko.uk)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -13,7 +28,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.topiello.lexicon.scanning;
 
