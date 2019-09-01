2820edea568d42b62e3950360de0e7fd5af8d9e5
Elias N Vasylenko <eliasvasylenko@gmail.com>
2019-09-01 13:16:19 +0100

    added tests for empty cursors

--- scanner/src/test/java/org/topiello/scanner/CursorTest.java
+++ scanner/src/test/java/org/topiello/scanner/CursorTest.java
@@ -1,8 +1,40 @@
 package org.topiello.scanner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CursorTest {
   @Test
-  testDefaultConstructorProducesEmptyCursor() {
-    
+  void testDefaultConstructorProducesEmptyCursor() {
+    var cursor = new Cursor<>();
+
+    Assertions.assertTrue(cursor.isEndOfInput());
+  }
+
+  @Test
+  void testEmptyCursorGetsDefault() {
+    var cursor = new Cursor<Object>();
+
+    var defaultObject = new Object();
+
+    Assertions.assertEquals(defaultObject, cursor.characterOrElse(defaultObject));
+  }
+
+  @Test
+  void testEmptyCursorThrowsOnGet() {
+    var cursor = new Cursor<>();
+
+    var throwable = new Throwable() {
+      private static final long serialVersionUID = 1L;
+    };
+
+    Assertions.assertThrows(throwable.getClass(), () -> cursor.characterOrThrow(() -> throwable));
+  }
+
+  @Test
+  void testEmptyCursorDoesNotMachPredicate() {
+    var cursor = new Cursor<>();
+
+    Assertions.assertFalse(cursor.characterMatches(c -> true));
   }
 }
